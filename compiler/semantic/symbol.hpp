380331b076cae231cd77e3ee2159237c54b17f1f
leod <subtle.frustration@proton.me>
2010-07-29 01:45:21 +0200

    add semantic files

---
+++ compiler/semantic/symbol.hpp
@@ -0,0 +1,14 @@
+#ifndef LLANG_SEMANTIC_SYMBOL_HPP_INCLUDED
+#define LLANG_SEMANTIC_SYMBOL_HPP_INCLUDED
+
+namespace llang {
+namespace semantic {
+
+class Symbol {
+
+};
+
+} // namespace semantic
+} // namespace llang
+
+#endif
