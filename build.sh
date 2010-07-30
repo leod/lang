59af544dae89a2cc681120bb7975ebcdf6f260c0
Leonard Dahlmann <leo.dahlmann@gmail.com>
2010-07-30 19:50:52 +0200

    Fix segmentation fault in ExpressionVisitor.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm -f a.out
-find -name '*.cpp' | xargs gcc -Icompiler -lstdc++ -Wall -g -pedantic -Wextra -Wformat -Wconversion -std=c++0x
+find -name '*.cpp' | xargs gcc -Icompiler -lstdc++ -Wall -g -pedantic -Wextra -Wformat -Wconversion -std=c++0x || exit $?
