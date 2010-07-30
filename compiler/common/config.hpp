e5ad199bca52ce948b762c96a219e148472604d5
leod <subtle.frustration@proton.me>
2010-07-30 18:54:08 +0200

    add type checking in function calls. add files i forgot to add earlier.

---
+++ compiler/common/config.hpp
@@ -0,0 +1,16 @@
+#ifndef LLANG_COMMON_CONFIG_HPP_INCLUDED
+#define LLANG_COMMON_CONFIG_HPP_INCLUDED
+
+#include <string>
+
+namespace llang {
+
+// will later contain things like include paths
+struct Config {
+
+};
+
+} // namespace llang
+
+#endif
+
