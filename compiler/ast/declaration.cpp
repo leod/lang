aba49c2aa3afcc62f6081f5b3c47cfa612dbb6a2
Leonard Dahlmann <leo.dahlmann@gmail.com>
2010-07-31 01:56:43 -0700

    Forgot to add some files.

---
+++ compiler/ast/declaration.cpp
@@ -0,0 +1,33 @@
+#include "ast/type.hpp"
+#include "ast/expression.hpp"
+#include "ast/declaration.hpp"
+
+namespace llang {
+namespace ast {
+
+FunctionDeclaration::FunctionDeclaration(const Location& location,
+                                         Type* returnType,
+                                         const identifier_t& name,
+                                         parameter_list_t& parameters,
+                                         Expression* body)
+	: Declaration(Node::FUNCTION_DECLARATION, location),
+	  returnType(returnType),
+	  name(name),
+	  parameters(parameters),
+	  body(body) {}
+
+FunctionDeclaration::~FunctionDeclaration() {}
+
+VariableDeclaration::VariableDeclaration(const Location& location,
+                                         Type* type,
+                                         const identifier_t& name,
+                                         Expression* initializer)
+	: Declaration(Node::VARIABLE_DECLARATION, location),
+	  type(type),
+	  name(name),
+	  initializer(initializer) {}
+
+VariableDeclaration::~VariableDeclaration() {}
+
+} // namespace ast
+} // namespace llang
